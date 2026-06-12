692ca64b128595392afa323d978157b34e65ed0f
KoenKaandorp <k.d.kaandorp@student.tue.nl>
2026-06-12 09:19:53 +0200

    stats update

--- js/views/stats.js
+++ js/views/stats.js
@@ -3,7 +3,7 @@
 
 import { levelDistribution, dailyProgress, comprehensionOverTime, coverageEstimate } from '../stats.js';
 
-const LEVEL_LABELS = ['New', 'Learning', 'Young', 'Maturing', 'Mature'];
+const LEVEL_LABELS = ['New', 'Learning', 'Familiar', 'Known', 'Mastered'];
 
 export function mountStatsView(root) {
   root.innerHTML = `
@@ -64,7 +64,7 @@ function renderCoverage(el, { coverage, knownCount, total }) {
     <div class="coverage__bar" role="progressbar" aria-valuenow="${pct}" aria-valuemin="0" aria-valuemax="100">
       <div class="coverage__fill" style="width:${pct}%"></div>
     </div>
-    <p class="coverage__meta">${knownCount} of ${total} seeded words mastered</p>
+    <p class="coverage__meta">${knownCount} of ${total} words mastered</p>
   `;
 }
 
